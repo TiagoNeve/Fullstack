93cab3b6ff0dbf09871a142b0fed68a91d429b6c
TiagoNeve <69875628+TiagoNeve@users.noreply.github.com>
2022-11-20 09:01:00 -0300

    pizzaLegends Init

--- game/primeiroProjeto/js/classes.js
+++ game/primeiroProjeto/js/classes.js
@@ -13,3 +13,49 @@ class Sprite {
         ctx.fill();
     }
 }
+
+class Player extends Sprite {
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color);
+        this.s1 = new Sphere(this.x + Math.cos(0) * this.radius,
+                             this.y + Math.sin(0) * this.radius,
+                             2,
+                             '#48FCFF',
+                             .08,
+                             this)
+    }
+
+    // subscrevendo
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+    }
+
+    update() {
+        this.draw();
+        this.s1.update();
+    }
+}
+
+class Sphere extends Sprite {
+    constructor(x, y, radius, color, angleUpdateValue, player) {
+        super(x, y, radius, color);
+        this.angleUpdateValue = angleUpdateValue;
+        this.player = 0;
+    }
+
+    // subscrevendo
+    update() {
+        this.draw();
+        this.angle += this.angleUpdateValue;
+        if(Math.abs(this.angle) >= Math.PI*2) {
+            this.angle = 0;
+        }
+
+        this.x = this.player.x + Math.cos(this.angle) * this.player.radius;
+        this.y = this.player.y + Math.sin(this.angle) * this.player.radius;
+    }
+    
+}
--- game/primeiroProjeto/js/script.js
+++ game/primeiroProjeto/js/script.js
@@ -6,7 +6,7 @@ cnv.height = innerHeight; // Pega todo o tamanho da janela
 
 const ctx = cnv.getContext('2d');
 
-const player = new Sprite(cnv.width/2, cnv.height/2, 30, 'white')
+const player = new Player(cnv.width/2, cnv.height/2, 30, '#48FCFF');
 
 function loop() {
     requestAnimationFrame(loop, cnv); // Vai tentar executar algum animação, tentar manter uma taxa de execução de 60 fps
