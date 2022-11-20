7598a9a29ad8512cef8c69ff52f9b7332e179621
TiagoNeve <69875628+TiagoNeve@users.noreply.github.com>
2022-11-19 21:23:58 -0300

    bola branca

--- game/primeiroProjeto/js/script.js
+++ game/primeiroProjeto/js/script.js
@@ -6,6 +6,8 @@ cnv.height = innerHeight; // Pega todo o tamanho da janela
 
 const ctx = cnv.getContext('2d');
 
+const player = new Sprite(cnv.width/2, cnv.height/2, 30, 'white')
+
 function loop() {
     requestAnimationFrame(loop, cnv); // Vai tentar executar algum animação, tentar manter uma taxa de execução de 60 fps
     update();
@@ -14,6 +16,8 @@ function loop() {
 function update() {
     ctx.fillStyle = 'rgba(0,0,0,1)';
     ctx.fillRect(0,0,cnv.width,cnv.height);
+    
+    player.draw();
 }
 
 loop();
